373cc33204d9cf0cf3c2a4aaf3b33b270d6bf27b
wert2024 <158856496+wert2024@users.noreply.github.com>
2024-06-20 18:46:55 +0300

    Add files via upload

---
+++ калькулятор.js
@@ -0,0 +1,38 @@
+
+
+
+
+		  function countBtn() {
+let count = document.getElementById('count');
+let value = count.innerHTML;
+++value;
+console.log(value);
+document.getElementById('count').innerHTML = value;
+			  
+		  }
+     
+      
+      
+       
+ 
+     
+     
+     
+ 
+     
+     
+
+
+
+  
+     
+
+
+
+
+
+
+
+
+
+
